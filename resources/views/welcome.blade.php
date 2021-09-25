5d60183ca1028d02ef61c91a64ef563dbd415b6d
Miloš Terzić <terzic.milos.tm@gmail.com>
2021-09-25 20:01:19 +0200

    Adding routes to index page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,60 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="{{ asset('css/main.css') }}" rel="stylesheet">
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -82,14 +29,14 @@
 
                 <div class="links">
                     @auth()
-                        <a href="#">Create thread</a>
+                        <a href="{{ route('threads.create') }}">Create thread</a>
                     @endauth
                 </div>
 
                 <div>
                     @foreach($threads as $thread)
-                        <div class="thread-wrapper" style="text-align: left">
-                            <a href="{{ route('threads.show', [$thread->id]) }}"><h2>{{ $thread->title }}</h2></a>
+                        <div class="thread-wrapper">
+                            <a href="{{ route('threads.show', [$thread->id]) }}"><h2>{{ $thread->title }} <span>{{ '(' . $thread->user->name . ')' }}</span></h2></a>
                             <p>
                                 @auth
                                     @if(Auth::user()->isOwner($thread->id))
@@ -100,6 +47,8 @@
                                         <a href="{{ route('threads.destroy', [$thread->id]) }}">DELETE</a>
                                     @endif
                                 @endauth
+
+                                <hr>
                             </p>
                         </div>
                     @endforeach
