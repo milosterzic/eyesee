0245c78e9c0c96b99d85ebe64c3434079fa2a155
Miloš Terzić <terzic.milos.tm@gmail.com>
2021-09-25 22:17:22 +0200

    Adding layout to welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,15 +9,7 @@
             </div>
         @endif
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ route('logout') }}">Logout, {{ Auth::user()->name }}</a>
-                    @else
-                        <a href="{{ route('login') }}">Login with Reddit</a>
-                    @endauth
-                </div>
-            @endif
+            @include('layout.navigation')
 
             <div class="content">
                 <div class="title m-b-md">
