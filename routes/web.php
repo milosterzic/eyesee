7996ba11a774caecb8bab3930b3559a10fca4980
Miloš Terzić <terzic.milos.tm@gmail.com>
2021-09-25 20:27:39 +0200

    Fixing routes

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,10 @@ Route::group(['prefix' => '/auth', 'name' => 'auth'], function () {
     Route::get('logout', 'LoginController@logout')->name('logout');
 });
 
-Route::resource('threads', 'ThreadsController')->only([
-    'index', 'show'
-]);
-
 Route::resource('threads', 'ThreadsController')->except([
     'index', 'show'
 ])->middleware('auth');
+
+Route::resource('threads', 'ThreadsController')->only([
+    'index', 'show'
+]);
