39bef82beae2cf3a672ea6fa6775d922a5fe0d00
Miloš Terzić <terzic.milos.tm@gmail.com>
2021-09-26 11:07:04 +0200

    Adding functionality for approving comments to blade files

--- resources/views/layout/showComments.blade.php
+++ resources/views/layout/showComments.blade.php
@@ -1,16 +1,30 @@
 @foreach($commentable->comments as $comment)
-    <div class="comment-wrapper">
-        <p>
-            {{ $comment->text }}
-        </p>
-        <p><i>{{ $comment->user->name . ', ' . $comment->created_at->format('d.M.Y. H:i:s') }}</i></p>
+    @if($comment->isApproved() || optional(Auth::user())->isOwner($rootThreadId))
+        <div class="comment-wrapper">
+            <p>
+                {{ $comment->text }}
+            </p>
+            <p>
+                <i>{{ $comment->user->name . ', ' . $comment->created_at->format('d.M.Y. H:i:s') }}</i>
 
-        @if ($comment->hasComments())
-            @include('layout.showComments', ['commentable' => $comment])
-        @endif
+                @if (! $comment->isApproved())
+                    <form method="post" action="{{ route('comments.update', ['comment' => $comment->id]) }}" class="form btn-link-0">
+                        @method('PUT')
+                        @csrf
 
-        @include('layout.comment', ['commentableId' => $comment->id, 'commentableType' => 'comment'])
-    </div>
+                        <input type="hidden" name="root_thread_id" value="{{ $rootThreadId }}">
+                        <button type="submit" class="btn btn-link btn-link-0">(Approve)</button>
+                    </form>
+                @endif
+            </p>
 
+            @if ($comment->hasApprovedComments() || optional(Auth::user())->isOwner($rootThreadId))
+                @include('layout.showComments', ['commentable' => $comment, 'rootThreadId' => $rootThreadId])
+            @endif
+
+            @if($comment->isApproved())
+                @include('layout.comment', ['commentableId' => $comment->id, 'commentableType' => 'comment'])
+            @endif
+        </div>
+    @endif
 @endforeach
-
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -2,32 +2,38 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 @include('layout.header')
 
-<body>
-<div class="flex-center position-ref">
-    @include('layout.navigation')
+    <body>
+        @if(session()->has('message'))
+            <div class="alert alert-success">
+                {{ session()->get('message') }}
+            </div>
+        @endif
+        <div class="flex-center position-ref">
 
-    <div class="content show">
-        <div class="title m-b-md">
-            EyeSee
+            @include('layout.navigation')
+
+            <div class="content show">
+                <div class="title m-b-md">
+                    EyeSee
+                </div>
+
+                <div class="links margin-bottom">
+                    <a href="{{ route('welcome') }}">All threads</a>
+                </div>
+
+                <div class="thread-wrapper show">
+                    <h2>{{ $thread->title }} <span>{{ '(' . $thread->user->name . ')' }}</span></h2>
+                    <p>
+                        {{ $thread->text }}
+                    </p>
+
+                    @if ($thread->hasApprovedComments() || optional(Auth::user())->isOwner($thread->id))
+                        @include('layout.showComments', ['commentable' => $thread, 'rootThreadId' => $thread->id])
+                    @endif
+
+                    @include('layout.comment', ['commentableId' => $thread->id, 'commentableType' => 'thread'])
+                </div>
+            </div>
         </div>
-
-        <div class="links margin-bottom">
-            <a href="{{ route('welcome') }}">All threads</a>
-        </div>
-
-        <div class="thread-wrapper show">
-            <h2>{{ $thread->title }} <span>{{ '(' . $thread->user->name . ')' }}</span></h2>
-            <p>
-                {{ $thread->text }}
-            </p>
-
-            @if ($thread->hasComments())
-                @include('layout.showComments', ['commentable' => $thread])
-            @endif
-
-            @include('layout.comment', ['commentableId' => $thread->id, 'commentableType' => 'thread'])
-        </div>
-    </div>
-</div>
-</body>
+    </body>
 </html>
